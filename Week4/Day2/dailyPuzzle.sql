17b6967e5a645f30a04b0f747690a20f24ecf48c
Kachi <kachiafo@gmail.com>
2023-08-01 11:00:18 +0300

    4th commit2.1

--- Week4/Day2/dailyPuzzle.sql
+++ Week4/Day2/dailyPuzzle.sql
@@ -4,7 +4,7 @@
 
 -- CREATE TABLE IF NOT EXISTS public.puzzle
 -- (
--- )
+-- 
 
 -- TABLESPACE pg_default;
 
@@ -33,26 +33,27 @@
 -- (NULL)
 
 
--- 
+-- SELECT * FROM SecondTab
+
 
 -- Q1
 -- SELECT COUNT(*) 
 -- FROM FirstTab AS ft WHERE ft.id NOT IN (SELECT id FROM SecondTab WHERE id IS NULL)
--- It prints the count of FirstTab id that does not equal to null.
+-- It prints the count of FirstTab id that does not equal to null. All firstTab rows execpt for NULL row.
 
 
 -- Q2
 -- SELECT COUNT(*) 
 -- FROM FirstTab AS ft WHERE ft.id NOT IN ( SELECT id FROM SecondTab WHERE id = 5 )
--- It prints the FirstTab item that has the SecondTab id of 5.
+-- It prints the FirstTab item that has the SecondTab id of 5. All firstTab rows execpt for '5' row.
 
 
 -- Q3
 -- SELECT COUNT(*) 
 --     FROM FirstTab AS ft WHERE ft.id NOT IN ( SELECT id FROM SecondTab )
--- It prints the FirstTab item that doesn't have a SecondTab id.
+-- It prints the FirstTab item that doesn't have a SecondTab id. All firstTab rows execpt for rows with secondTab ids.
 
 -- Q4
 -- SELECT COUNT(*) 
 --     FROM FirstTab AS ft WHERE ft.id NOT IN ( SELECT id FROM SecondTab WHERE id IS NOT NULL )
--- It prints the FirstTab item that doesn't have the SecondTab id of NULL.	
+-- It prints the FirstTab item that doesn't have the SecondTab id of NULL. All firstTab rows execpt for '5' row. Only prints NULL row.	
